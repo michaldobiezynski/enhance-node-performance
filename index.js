ce2e41381b6f692d15ab51c730febb476ca0a231
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-02-22 20:16:24 +0000

    PM2 configuration

--- index.js
+++ index.js
@@ -1,25 +1,16 @@
-const cluster = require("cluster");
 const crypto = require("crypto");
-process.env.UV_THREADPOOL_SIZE = 1;
+const express = require("express");
 
-console.log(cluster.isMaster);
+const app = express();
 
-if (cluster.isMaster) {
-  cluster.fork();
-  cluster.fork();
-} else {
-  const express = require("express");
-  const app = express();
-
-  app.get("/", (req, res) => {
-    crypto.pbkdf2("a", "b", 10000, 512, "sha512", () => {
-      res.send("Hi there");
-    });
+app.get("/", (req, res) => {
+  crypto.pbkdf2("a", "b", 10000, 512, "sha512", () => {
+    res.send("Hi there");
   });
+});
 
-  app.get("/fast", (req, res) => {
-    res.send("This was fast");
-  });
+app.get("/fast", (req, res) => {
+  res.send("This was fast");
+});
 
-  app.listen(3000);
-}
+app.listen(3000);
