948dd91cfe5b1fe691468c9cf2a5c614262baa7b
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-02-22 19:50:18 +0000

    benchmark refactor

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
 const cluster = require("cluster");
+const crypto = require("crypto");
+process.env.UV_THREADPOOL_SIZE = 1;
 
 console.log(cluster.isMaster);
 
 if (cluster.isMaster) {
   cluster.fork();
-  //   cluster.fork();
-  //   cluster.fork();
-  //   cluster.fork();
 } else {
   const express = require("express");
   const app = express();
 
-  function doWork(duration) {
-    const start = Date.now();
-    while (Date.now() - start < duration) {}
-  }
-
   app.get("/", (req, res) => {
-    doWork(5000);
-    res.send("Hi there");
+    crypto.pbkdf2("a", "b", 10000, 512, "sha512", () => {
+      res.send("Hi there");
+    });
   });
 
   app.get("/fast", (req, res) => {
